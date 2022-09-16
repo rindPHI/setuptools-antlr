4744e1940767d1f67137b6e49fc0b695522c4865
Dominic Steinhöfel <dominic.steinhoefel@cispa.de>
2022-09-16 09:47:18 +0200

    Upgrade to ANTLR 4.11.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
         name='setuptools-antlr',
         version='0.4.0',
         packages=setuptools.find_packages(),
-        package_data={'setuptools_antlr': ['lib/antlr-4.10.1-complete.jar', 'lib/LICENSE.txt']},
+        package_data={'setuptools_antlr': ['lib/antlr-4.11.1-complete.jar', 'lib/LICENSE.txt']},
         entry_points={
             'distutils.commands': [
                 'antlr = setuptools_antlr.command:AntlrCommand'
